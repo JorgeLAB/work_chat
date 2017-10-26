779c9666d34b894f5402f371badf241042af8f20
Daniel Moreto <dfmoreto@gmail.com>
2017-10-26 20:19:36 -0200

    Add schema and shotcut bash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530222634) do
+ActiveRecord::Schema.define(version: 20171026024351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20170530222634) do
     t.datetime "updated_at", null: false
     t.index ["team_id"], name: "index_channels_on_team_id", using: :btree
     t.index ["user_id"], name: "index_channels_on_user_id", using: :btree
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_invites_on_team_id", using: :btree
+    t.index ["user_id"], name: "index_invites_on_user_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
@@ -79,6 +89,8 @@ ActiveRecord::Schema.define(version: 20170530222634) do
 
   add_foreign_key "channels", "teams"
   add_foreign_key "channels", "users"
+  add_foreign_key "invites", "teams"
+  add_foreign_key "invites", "users"
   add_foreign_key "talks", "teams"
   add_foreign_key "team_users", "teams"
   add_foreign_key "team_users", "users"
